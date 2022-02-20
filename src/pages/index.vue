663bd6838b91a471dc08c30a8b7abce2a6f19f2a
Derin Önder Eren <derinondereren@gmail.com>
2022-02-20 11:55:37 +0300

    Update: Edit Meta tags

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,13 +6,16 @@
 
             <Meta name="twitter:card" content="summary_large_image" />
             <Meta content="d3r1n's Website" property="og:title" />
-            <Meta content="My Website built with Nuxt3 & Vue3" property="og:description" />
+            <Meta :content="`Hello! ${['👋', '👾', '👽', '🤖', '🎃', '👊', '👉👈', '💻', '🤓'][Math.floor(Math.random() * ['👋', '👾', '👽', '🤖', '🎃', '👊', '👉👈', '💻', '🤓'].length)]}\nI'm d3r1n, 15 y.o Student, Developer & Athlete.\nI'm crazy about Programming, Maths, Physics, Electronics and Sports.`" property="og:description" />
             <Meta content="d3r1n.com" property="og:site_name" />
             <Meta name="theme-color" content="#50fa7b" />
             <Meta content='/images/Background.png' property='og:image' itemprop="image" />
             <Link rel="icon" type="image/x-icon" href="/images/favicon.png" />
 
-            <Title>d3r1n</Title>
+            <Meta property="og:image:width" content="300" />
+            <Meta property="og:image:height" content="200">
+
+            <Title>Home - d3r1n</Title>
 
         </Head>
 
